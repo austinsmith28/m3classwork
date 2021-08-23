2ba1a1b2d21bda39aeb1d0f8c3f5896f3e7fb5f1
Austin Smith <flubahdub@gmail.com>
2021-08-23 12:45:56 -0400

    Hello World in Eclipse

--- HelloWorld/src/main/java/com/ajs/HelloWorld/Hello.java
+++ HelloWorld/src/main/java/com/ajs/HelloWorld/Hello.java
@@ -1,8 +1,16 @@
+/**
+ * @author Austin Smith
+ * email: austinsmith0523@gmail.com
+ * date: 08/23/2021
+ * purpose: My first hello world program in an IDE with maven
+ */
+
 package com.ajs.HelloWorld;
 
 public class Hello {
 	
 	public static void main (String[] args) {
+		
 		System.out.println("Hello, World!");
 		
 	}
